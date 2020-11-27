a5d69cdd8af75ae61b2dcf7185fe7590c039a6bd
Mateusz Krasucki <mateusz@ccpgames.com>
2020-11-27 16:54:16 +0000

    UPD `esi-lib` to 1.7.9.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email="",
     url="",
     install_requires=[
-        "esi >= 1.7.2"
+        "esi >= 1.7.9"
         if os.getenv("USE_INSTALLED_ESI_VERSION", 0) != "1" else "esi",
         "fibonacci-heap-mod",
     ],
